ecbfc38a8e0b73706fccf66207cc74c1b746fee8
Matt Fewer <mattfewer@hotmail.com>
2017-12-09 20:21:05 +0100

    acceptable build scripts working with webpack, babel, and nodemon

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("This is going to be great!");
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const path = require('path')
+
+app.use(express.static('public'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log("listening on port " + PORT)
+})
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+
+const DIST_DIR = path.resolve(__dirname, 'dist')
+const SRC_DIR = path.resolve(__dirname, 'src')
+
+const config = {
+    entry: './src/index.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                include: SRC_DIR,
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        'react',
+                        'stage-0',
+                        [
+                            'env', {
+                                targets: { browsers: ['last 2 versions'] }
+                            }
+                        ]
+                    ]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'css-loader' }
+                ]
+            }
+        ]
+    }
+};
+
+module.exports = config
