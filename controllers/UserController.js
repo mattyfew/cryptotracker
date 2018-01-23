036282658608fa3978a5bc4ee8e16c035205e798
nataschaberg <natalja.wittenberg@gmx.de>
2018-01-23 05:53:51 +0100

    api router added and logic defined for get, getById, and post

---
+++ controllers/UserController.js
@@ -0,0 +1,40 @@
+import User from '../db/models/user'
+
+export default = {
+  get: (params) => {
+    return new Promise((resolve, reject) => {
+      User.find(params, (err, users) => {
+        if(err) {
+          reject(err)
+        } else {
+          resolve(users)
+        }
+      })
+    })
+  },
+
+  getById: (id) => {
+    return new Promise((resolve, reject) => {
+      User.findById(id, (err, user) => {
+        if(err) {
+          reject(err)
+        } else {
+          resolve(user)
+        }
+      })
+    })
+  },
+
+  post: (params) => {
+    return new Promise((resolve, reject) => {
+      // TODO logic to hash important info
+      User.create(params, (err, user) => {
+        if(err) {
+          reject(err)
+        } else {
+          resolve(user)
+        }
+      })
+    })
+  }
+}
